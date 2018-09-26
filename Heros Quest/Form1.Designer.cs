3f8558c6822b59db12489db7884017b5dbd55ba7
Tyler Pogson <tylerpogson@outlook.com>
2018-09-26 17:37:38 -0400

    Add project files.

---
+++ Heros Quest/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Heros_Quest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.choseLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.easyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.easyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.easyButton.ForeColor = System.Drawing.Color.White;
+            this.easyButton.Location = new System.Drawing.Point(33, 259);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(116, 54);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "EASY";
+            this.easyButton.UseVisualStyleBackColor = true;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.mediumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mediumButton.ForeColor = System.Drawing.Color.White;
+            this.mediumButton.Location = new System.Drawing.Point(222, 259);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(116, 54);
+            this.mediumButton.TabIndex = 1;
+            this.mediumButton.Text = "MEDIUM";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButton_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.hardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.hardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hardButton.ForeColor = System.Drawing.Color.White;
+            this.hardButton.Location = new System.Drawing.Point(407, 259);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(116, 54);
+            this.hardButton.TabIndex = 2;
+            this.hardButton.Text = "HARD";
+            this.hardButton.UseVisualStyleBackColor = true;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(132, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // usernameInput
+            // 
+            this.usernameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameInput.Location = new System.Drawing.Point(222, 112);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(201, 22);
+            this.usernameInput.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(178, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Your Difficulty.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(90, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(380, 48);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hero\'s Quest";
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.Color.White;
+            this.welcomeLabel.Location = new System.Drawing.Point(215, 346);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(0, 24);
+            this.welcomeLabel.TabIndex = 7;
+            // 
+            // choseLabel
+            // 
+            this.choseLabel.AutoSize = true;
+            this.choseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choseLabel.ForeColor = System.Drawing.Color.White;
+            this.choseLabel.Location = new System.Drawing.Point(29, 389);
+            this.choseLabel.Name = "choseLabel";
+            this.choseLabel.Size = new System.Drawing.Size(0, 24);
+            this.choseLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.choseLabel);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernameInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hero\'s Quest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label choseLabel;
+    }
+}
+
---
+++ Heros Quest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heros_Quest
+{
+    public partial class Form1 : Form
+    {
+        string userName;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            userName = usernameInput.Text;
+
+            welcomeLabel.Text = "Welcome " + userName;
+            choseLabel.Text = "A true brave Hero! Go forth and conquer the easy path!";
+        }
+
+        private void mediumButton_Click(object sender, EventArgs e)
+        {
+            userName = usernameInput.Text;
+
+            welcomeLabel.Text = "Welcome " + userName;
+            choseLabel.Text = "A true brave Hero! Go forth and conquer the medium path!";
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+            userName = usernameInput.Text;
+
+            welcomeLabel.Text = "Welcome " + userName;
+            choseLabel.Text = "A true brave Hero! Go forth and conquer the hard path!";
+        }
+    }
+}
