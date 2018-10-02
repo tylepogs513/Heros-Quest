002cc739d0011a46024d8feefebb6674d2291812
Tyler Pogson <tylerpogson@outlook.com>
2018-10-01 22:58:27 -0400

    Final

--- Heros Quest/Form1.Designer.cs
+++ Heros Quest/Form1.Designer.cs
@@ -32,12 +32,15 @@
             this.easyButton = new System.Windows.Forms.Button();
             this.mediumButton = new System.Windows.Forms.Button();
             this.hardButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
             this.usernameInput = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.difLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
             this.welcomeLabel = new System.Windows.Forms.Label();
             this.choseLabel = new System.Windows.Forms.Label();
+            this.startButt = new System.Windows.Forms.Button();
+            this.resetButt = new System.Windows.Forms.Button();
+            this.errLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // easyButton
@@ -56,8 +59,8 @@
             // 
             // mediumButton
             // 
-            this.mediumButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.mediumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.mediumButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Orange;
+            this.mediumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.mediumButton.ForeColor = System.Drawing.Color.White;
             this.mediumButton.Location = new System.Drawing.Point(222, 259);
@@ -82,16 +85,16 @@
             this.hardButton.UseVisualStyleBackColor = true;
             this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
             // 
-            // label1
+            // userLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(132, 115);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 16);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Username:";
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.ForeColor = System.Drawing.Color.White;
+            this.userLabel.Location = new System.Drawing.Point(132, 115);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(83, 16);
+            this.userLabel.TabIndex = 3;
+            this.userLabel.Text = "Username:";
             // 
             // usernameInput
             // 
@@ -101,27 +104,27 @@
             this.usernameInput.Size = new System.Drawing.Size(201, 22);
             this.usernameInput.TabIndex = 4;
             // 
-            // label2
+            // difLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(178, 186);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(207, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Select Your Difficulty.";
+            this.difLabel.AutoSize = true;
+            this.difLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difLabel.ForeColor = System.Drawing.Color.White;
+            this.difLabel.Location = new System.Drawing.Point(178, 186);
+            this.difLabel.Name = "difLabel";
+            this.difLabel.Size = new System.Drawing.Size(207, 24);
+            this.difLabel.TabIndex = 5;
+            this.difLabel.Text = "Select Your Difficulty.";
             // 
-            // label3
+            // nameLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(90, 48);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(380, 48);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Hero\'s Quest";
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.nameLabel.Location = new System.Drawing.Point(90, 48);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(380, 48);
+            this.nameLabel.TabIndex = 6;
+            this.nameLabel.Text = "Hero\'s Quest";
             // 
             // welcomeLabel
             // 
@@ -143,24 +146,70 @@
             this.choseLabel.Size = new System.Drawing.Size(0, 24);
             this.choseLabel.TabIndex = 8;
             // 
+            // startButt
+            // 
+            this.startButt.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.startButt.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.startButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButt.ForeColor = System.Drawing.Color.Blue;
+            this.startButt.Location = new System.Drawing.Point(240, 412);
+            this.startButt.Name = "startButt";
+            this.startButt.Size = new System.Drawing.Size(97, 30);
+            this.startButt.TabIndex = 9;
+            this.startButt.Text = "Start";
+            this.startButt.UseVisualStyleBackColor = true;
+            this.startButt.Click += new System.EventHandler(this.startButt_Click);
+            // 
+            // resetButt
+            // 
+            this.resetButt.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.resetButt.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.resetButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButt.ForeColor = System.Drawing.Color.Blue;
+            this.resetButt.Location = new System.Drawing.Point(231, 259);
+            this.resetButt.Name = "resetButt";
+            this.resetButt.Size = new System.Drawing.Size(97, 30);
+            this.resetButt.TabIndex = 10;
+            this.resetButt.Text = "Reset";
+            this.resetButt.UseVisualStyleBackColor = true;
+            this.resetButt.Click += new System.EventHandler(this.resetButt_Click);
+            // 
+            // errLabel
+            // 
+            this.errLabel.AutoSize = true;
+            this.errLabel.Font = new System.Drawing.Font("OCRA", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errLabel.ForeColor = System.Drawing.Color.White;
+            this.errLabel.Location = new System.Drawing.Point(30, 23);
+            this.errLabel.MinimumSize = new System.Drawing.Size(493, 400);
+            this.errLabel.Name = "errLabel";
+            this.errLabel.Size = new System.Drawing.Size(493, 400);
+            this.errLabel.TabIndex = 11;
+            this.errLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.resetButt);
+            this.Controls.Add(this.errLabel);
+            this.Controls.Add(this.startButt);
             this.Controls.Add(this.choseLabel);
             this.Controls.Add(this.welcomeLabel);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.difLabel);
             this.Controls.Add(this.usernameInput);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userLabel);
             this.Controls.Add(this.hardButton);
             this.Controls.Add(this.mediumButton);
             this.Controls.Add(this.easyButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Hero\'s Quest";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,12 +220,15 @@
         private System.Windows.Forms.Button easyButton;
         private System.Windows.Forms.Button mediumButton;
         private System.Windows.Forms.Button hardButton;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label userLabel;
         private System.Windows.Forms.TextBox usernameInput;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label difLabel;
+        private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label welcomeLabel;
         private System.Windows.Forms.Label choseLabel;
+        private System.Windows.Forms.Button startButt;
+        private System.Windows.Forms.Button resetButt;
+        private System.Windows.Forms.Label errLabel;
     }
 }
 
--- Heros Quest/Form1.cs
+++ Heros Quest/Form1.cs
@@ -19,28 +19,73 @@ namespace Heros_Quest
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetButt.Visible = false;
+            errLabel.Visible = false;
+        }
+
         private void easyButton_Click(object sender, EventArgs e)
         {
             userName = usernameInput.Text;
+            easyButton.BackColor = Color.Lime;
+            mediumButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Black;
 
-            welcomeLabel.Text = "Welcome " + userName;
+            welcomeLabel.Text = "Welcome " + userName + "!";
             choseLabel.Text = "A true brave Hero! Go forth and conquer the easy path!";
         }
 
         private void mediumButton_Click(object sender, EventArgs e)
         {
             userName = usernameInput.Text;
+            easyButton.BackColor = Color.Black;
+            mediumButton.BackColor = Color.Orange;
+            hardButton.BackColor = Color.Black;
 
-            welcomeLabel.Text = "Welcome " + userName;
+            welcomeLabel.Text = "Welcome " + userName + "!";
             choseLabel.Text = "A true brave Hero! Go forth and conquer the medium path!";
         }
 
         private void hardButton_Click(object sender, EventArgs e)
         {
             userName = usernameInput.Text;
+            easyButton.BackColor = Color.Black;
+            mediumButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Red;
 
-            welcomeLabel.Text = "Welcome " + userName;
+            welcomeLabel.Text = "Welcome " + userName + "!";
             choseLabel.Text = "A true brave Hero! Go forth and conquer the hard path!";
+        }
+
+        private void startButt_Click(object sender, EventArgs e)
+        {
+            easyButton.Visible = false;
+            mediumButton.Visible = false;
+            hardButton.Visible = false;
+            difLabel.Visible = false;
+            nameLabel.Visible = false;
+            userLabel.Visible = false;
+            usernameInput.Visible = false;
+            startButt.Visible = false;
+            resetButt.Visible = true;
+            errLabel.Visible = true;
+
+            errLabel.Text = "ERROR 404";
+        }
+
+        private void resetButt_Click(object sender, EventArgs e)
+        {
+            easyButton.Visible = true;
+            mediumButton.Visible = true;
+            hardButton.Visible = true;
+            difLabel.Visible = true;
+            nameLabel.Visible = true;
+            userLabel.Visible = true;
+            usernameInput.Visible = true;
+            startButt.Visible = true;
+            resetButt.Visible = false;
+            errLabel.Visible = false;
         }
     }
 }
